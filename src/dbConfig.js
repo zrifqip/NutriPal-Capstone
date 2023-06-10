8cdd3c0a474b5819ad6af0c8b5ef75b3b0fe11f0
18319027 <18319027@std.stei.itb.ac.id>
2023-06-10 10:10:06 +0700

    Update dbConfig.js
    Remember to replace it with your own database

--- src/dbConfig.js
+++ src/dbConfig.js
@@ -1,9 +1,10 @@
 const mysql = require('mysql');
+//TO DO: Replace it with your current database
 const db = mysql.createConnection({
-  host: 'localhost', // Replace with your host name
-  user: 'root',      // Replace with your database username
-  password: '',      // Replace with your database password
-  database: 'usercapstone' // // Replace with your database Name
+  host: 'localhost', 
+  user: 'root',      
+  password: '',      
+  database: 'usercapstone' 
 }); 
  
 db.connect(function(err) {
