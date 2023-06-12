c5d21b7a3f4410f1b4708c4cd1d1aa3d270d0efa
18319027 <18319027@std.stei.itb.ac.id>
2023-06-12 09:42:14 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 gcloud builds submit \
   --tag gcr.io/$GOOGLE_CLOUD_PROJECT/nutripal-survey-api:v0.1
 
-gcloud run deploy nutripal-auth-api \
+gcloud run deploy nutripal-survey-api \
   --image gcr.io/$GOOGLE_CLOUD_PROJECT/nutripal-survey-api:v0.1 \
   --platform managed \
   --region asia-southeast2 \
