049212718e633e9ad164d6946f35fb8d8dc727dd
Sitanggang Jerry Josia Partogi <79432841+Zeree17@users.noreply.github.com>
2023-06-15 19:57:23 +0700

    Update BMI
    Add new folder, 2 new py file, separate Mapping from main

---
+++ ML/BMI py/mappings.py
@@ -0,0 +1,56 @@
+# mappings.py
+
+# Define the mappings for categorical columns
+gender_mapping = {
+    'Female': 0,
+    'Male': 1
+}
+
+family_history_mapping = {
+    'no': 0,
+    'yes': 1
+}
+
+favc_mapping = {
+    'no': 0,
+    'yes': 1
+}
+
+caec_mapping = {
+    'no': 0,
+    'Sometimes': 1,
+    'Frequently': 2,
+    'Always': 3
+}
+
+smoke_mapping = {
+    'no': 0,
+    'yes': 1
+}
+
+scc_mapping = {
+    'no': 0,
+    'yes': 1
+}
+
+calc_mapping = {
+    'no': 0,
+    'Sometimes': 1,
+    'Frequently': 2,
+    'Always': 3
+}
+
+mtrans_mapping = {
+    'Public_Transportation': 0,
+    'Automobile': 1,
+    'Bike': 2,
+    'Motorbike': 3,
+    'Walking': 4
+}
+
+nobeyesdad_mapping = {
+    'Insufficient_Weight': 0,
+    'Normal_Weight': 1,
+    'Overweight': 2,
+    'Obessed': 3
+}
